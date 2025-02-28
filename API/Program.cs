0f262678d7830ec504fefc48093a08b265c28591
Aeraf Yousuf <aeraf11@gmail.com>
2025-02-28 11:07:16 +0000

    End of Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Application.Activities.Queries;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -8,6 +10,10 @@ builder.Services.AddDbContext<AppDbContext>(opt => {
 });
 
 builder.Services.AddCors();
+
+builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 // Add services to the container.
 
